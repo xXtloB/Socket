c5939c2473b3d5abbc2cb1a1ada6c348d3f39dd1
xXtloB <71756309+xXtloB@users.noreply.github.com>
2021-01-20 12:42:55 +0100

    livello2

--- client.py
+++ client.py
@@ -2,12 +2,18 @@
 
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-
-print('Received', data.decode())
+SERVER_ADDRESS = '127.0.0.1'  # The server's hostname or IP address
+SERVER_PORT = 65433        # The port used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((SERVER_ADDRESS, SERVER_PORT))
+dati = input("Inserisci messaggio per il server: ")
+dati = dati.encode()
+# Send data to server
+s.send(dati)
+# Receive response from server
+dati = s.recv(2048)
+if dati:
+    # Convert back to string for python3
+    dati = dati.decode()
+    print("Ho ricevuto dal server: ")
+    print(dati + '\n')
--- server.py
+++ server.py
@@ -5,15 +5,27 @@ import socket
 HOST = '127.0.0.1'  # Indirizzo dell'interfaccia standard di loopback (localhost)
 PORT = 65432        # Porta di ascolto, la lista di quelle utilizzabili parte da >1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print("[*] In ascolto su %s:%d" % (HOST, PORT))
-    clientsocket, address = s.accept()
-    with clientsocket as cs:
-        print('Connessione da', address)
-        while True:
-            data = cs.recv(1024)
-            if not data:
-                break
-            cs.sendall(data)
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Optionale: permette di riavviare subito il codice,
+# altrimenti bisognerebbe aspettare 2-4 minuti prima di poter riutilizzare(bindare) la stessa porta
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+s.bind((HOST, PORT))
+s.listen()
+print("[*] In ascolto su %s:%d" % (HOST, PORT))
+clientsocket, address = s.accept()
+with clientsocket as cs:
+    print('Connessione da', address)
+    while True:
+        dati = cs.recv(1024)
+        dati.decode()
+        if not dati:
+            break
+        dati = dati.decode()
+        print("Ricevuto '%s' dal client" % dati)
+        dati = "Ciao, " + str(address) + ". Ho ricevuto questo: '" + dati + "'"
+        dati = dati.encode()
+        # Invia i dati modificati al client
+        cs.send(dati)
+        print('Inviato al client:', dati)
