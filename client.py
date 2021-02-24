433b52a8ca2add62cb61d9aca0f542d4c141cc3b
xXtloB <andreagaravaglia37@gmail.com>
2021-02-24 12:51:41 +0100

    l7

--- client.py
+++ client.py
@@ -1,53 +1,42 @@
-#!/usr/bin/env python3
-
 import socket
+import sys
+import random
+import os
+import time
+import threading
+import multiprocessing
 
 SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 22224
-a
-def invia_comandi(sock_service):#Chiedo i dati al client in un ciclo e poi li invio al server
-    while True:
-        try:
-            n1 = input("Inserisci il primo numero: ")
-            n2 = input("Inserisci il secondo numero: ")
-            oper = input("Inserisci l'operazione da effettuare(piu / meno / per / diviso): ")
-            dati=f"{oper};{n1};{n2}"
+NUM_WORKERS=15
 
-        except EOFError:
-            print("\nOkay. Exit")
-            break
-        if not dati:
-            print("Non puoi inviare una stringa vuota!")
-            continue
-        if dati == '0':
-            print("Chiudo la connessione con il server!")
-            break
-        
-        dati = dati.encode()
+def genera_ricieste(address,port):
+    start_time_thread= time.time()
+    print(f"Client PID: {os.getpid()}, Process Name: {multiprocessing.current_process().name}, Thread Name: {threading.current_thread().name}")
+    try:
+        s=socket.socket()
+        s.connect((address,port))
+        print(f"{threading.current_thread().name} Connessione al server: {address}:{port}")
+    except s.error as errore:
+        print(f"{threading.current_thread().name} Qualcosa è andato storto, sto uscendo... \n{errore}")
+        sys.exit()
+    comandi=['piu','meno','per','diviso']
+    operazione=comandi[random.randint(0,3)]
+    dati=str(operazione)+";"+str(random.randint(1,100))+";"+str(random.randint(1,100))
+    dati=dati.encode()
+    s.send(dati)
+    dati=s.recv(2048)
+    if not dati:
+        print(f"{threading.current_thread().name}: Server non risponde. Exit")
+    dati=dati.decode()
+    print(f"{threading.current_thread().name}: Ricevuto dal server:")
+    print(dati + '\n')
+    dati="ko"
+    dati=dati.encode()
+    s.send(dati)
+    s.close()
+    end_time_thread=time.time()
+    print(f"{threading.current_thread().name} execution time=", end_time_thread-start_time_thread)
 
-        sock_service.send(dati)
-
-        dati = sock_service.recv(2048)
-
-        if not dati:
-            print("Server non risponde. Exit")
-            break
-        
-        dati = dati.decode()
-
-        print("Ricevuto dal server:")
-        print(dati + '\n')
-
-        sock_service.close()
-
-def connessione_server(address,port):#in questa funzione inserisco tutti i comandi riguardanti la connessione al server
-
-    sock_service = socket.socket()
-
-    sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
-
-    print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
-    invia_comandi(sock_service)
-
-if __name__=='__main__':
-    connessione_server(SERVER_ADDRESS,SERVER_PORT)
+if __name__ == '__main__':
+    print("ciao")
