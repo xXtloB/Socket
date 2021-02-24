d51d9134fe13a0003456db6f0b8106990ae8cffb
xXtloB <andreagaravaglia37@gmail.com>
2021-02-24 13:03:15 +0100

    finito senza commenti

--- client.py
+++ client.py
@@ -39,4 +39,22 @@ def genera_ricieste(address,port):
     print(f"{threading.current_thread().name} execution time=", end_time_thread-start_time_thread)
 
 if __name__ == '__main__':
-    print("ciao")
+    start_time=time.time()
+    for _ in range (0,NUM_WORKERS):
+        genera_ricieste(SERVER_ADDRESS, SERVER_PORT)
+    end_time=time.time()
+    print("Total SERIAL time=", end_time - start_time)
+
+    start_time=time.time()
+    threads=[threading.Thread(target=genera_ricieste, args=(SERVER_ADDRESS, SERVER_PORT,)) for _ in range(NUM_WORKERS)]
+    [thread.start() for thread in threads]
+    [thread.join() for thread in threads]
+    end_time=time.time()
+    print("Total THREADS time= ", end_time - start_time)
+
+    start_time=time.time()
+    processes =[multiprocessing.Process(target=genera_ricieste, args=(SERVER_ADDRESS, SERVER_PORT,)) for _ in range(NUM_WORKERS)]
+    [process.start() for process in processes]
+    [process.join() for process in processes]
+    end_time=time.time()
+    print("Total PROCESS time= ", end_time - start_time)
